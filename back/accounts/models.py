c457d3c42a214001b56a8f5bbc3ce0beab734cb9
cyanjz <yozdaz@gmail.com>
2025-05-23 08:27:59 +0900

    1st model impelements

--- back/accounts/models.py
+++ back/accounts/models.py
@@ -3,4 +3,8 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    user_age = models.PositiveIntegerField(null=True)
+    user_introduction = models.CharField(max_length=100, blank=True)
+    user_profile_img = models.CharField(max_length=50, blank=True)
+    user_banner_img = models.CharField(max_length=50, blank=True)
+    
