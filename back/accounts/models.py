758fd36538f93b0715f878145160a05b7c16a2eb
cyanjz <yozdaz@gmail.com>
2025-05-23 17:29:29 +0900

    be-auth-start

--- back/accounts/models.py
+++ back/accounts/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
+    first_name = None
+    last_name = None
     user_age = models.PositiveIntegerField(null=True)
     user_introduction = models.CharField(max_length=100, blank=True)
     user_profile_img = models.CharField(max_length=50, blank=True)
