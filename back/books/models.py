8addcb458c9683552bb29ba3a96428e3781a6c9f
cyanjz <yozdaz@gmail.com>
2025-05-24 14:26:04 +0900

    db_preparation_done

--- back/books/models.py
+++ back/books/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
+PATH = {
+    'Book' : {
+        'cover_image' : 'books/book_covers/',
+        'author_image' : 'books/author_images/',
+        'audio_file' : 'books/audios/',
+    },
+    'Thread' : {
+        'cover_image' : 'threads/thread_covers/',
+    }
+}
+
+
 # Create your models here.
 class Category(models.Model):
     category_name = models.CharField(max_length=20)
@@ -12,10 +24,12 @@ class Book(models.Model):
     book_title = models.CharField(max_length=50)
     book_description = models.TextField()
     book_ISBN13 = models.CharField(max_length=50)
-    book_cover_img = models.CharField(max_length=100)
+    book_cover_img = models.ImageField(upload_to=PATH['Book']['cover_image'], default='')
     book_publisher = models.CharField(max_length=50)
     book_pub_date = models.DateField()
-    book_customer_review_rank = models.IntegerField()
+    book_customer_review_rank = models.IntegerField(null=True)
+    book_ranking = models.PositiveIntegerField()
+    book_embedding = models.BinaryField(blank=True, null=True)
 
 
 class Thread(models.Model):
@@ -25,7 +39,7 @@ class Thread(models.Model):
     thread_title = models.CharField(max_length=40)  # max_length 20 -> 40
     thread_content = models.TextField()
     thread_book_review_rank = models.FloatField(default=0.0)
-    thread_cover_img = models.CharField(max_length=50, blank=True)
+    thread_cover_img = models.ImageField(upload_to=PATH['Thread']['cover_image'], default='')
     thread_created_at = models.DateTimeField(auto_now_add=True)
     thread_updated_at = models.DateTimeField(auto_now=True)
     # ThreadLikeUser -> thread_like_users
@@ -45,5 +59,6 @@ class Comment(models.Model):
 class Author(models.Model):
     author_name = models.CharField(max_length=20)   # varchar(15) -> varchar(20)
     author_info = models.TextField()
-    author_profile_img = models.CharField(max_length=50)  # text field -> char field
-    author_info_mp3 = models.CharField(max_length=50)
+    author_masterpeice = models.CharField(max_length=50)
+    author_profile_img = models.ImageField(blank=True, upload_to=PATH['Book']['author_image'], default='')
+    author_info_mp3 = models.FileField(blank=True, upload_to=PATH['Book']['audio_file'])
---
+++ back/books/utils_test.py
@@ -0,0 +1,4 @@
+from utils import OpenAiAPI
+
+ai_instance = OpenAiAPI()
+print(ai_instance.get_author_info('한강'))
